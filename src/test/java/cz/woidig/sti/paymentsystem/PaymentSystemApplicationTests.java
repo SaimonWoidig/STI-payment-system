a3dff39852f901d101114e20d05f332ffd8b0b07
Šimon Woidig <woidig.simon@gmail.com>
2024-03-06 12:42:34 +0100

    chore: init repo

---
+++ src/test/java/cz/woidig/sti/paymentsystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.woidig.sti.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
