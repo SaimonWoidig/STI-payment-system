54f54bb7ad71938525fcce132908be8043bb8cb5
Šimon Woidig <woidig.simon@gmail.com>
2024-04-03 13:49:29 +0200

    feat: added tests

--- src/test/java/cz/woidig/sti/paymentsystem/controller/PaymentControllerTest.java
+++ src/test/java/cz/woidig/sti/paymentsystem/controller/PaymentControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,7 @@ class PaymentControllerTest {
 
     @BeforeEach
     void setUp() {
+        Mockito.doNothing().when(paymentService).makePayment(Mockito.any(MakePaymentDTO.class));
         paymentController = new PaymentController(paymentService);
     }
 
